3a9c5882760c0e506b15de14a8932c0261ef8727
Sergey A. Volkov <sergey.a.volkov@gmail.com>
2024-01-21 18:15:36 +0300

    Add support for Tablespaces from RedDatabase 5

--- gstat2sql.py
+++ gstat2sql.py
@@ -77,6 +77,7 @@ class TableStat:
     def __init__(self):
         """Class init description here"""
         self.name = None
+        self.tabel_space = None
         self.formats_total = None
         self.formats_used = None
         self.rec_avg_len = None
@@ -112,6 +113,7 @@ class TableStat:
         self.fill_99 = None
         self.re = {
             'name': re.compile('^([^ ].+) \([0-9]+\)$'),
+            'table_space': re.compile('^ {4}Tablespace: (.+)$'),
             'ppp': re.compile('^ {4}Primary pointer page: ([0-9]+), Index root page: ([0-9]+)$'),
             'formats': re.compile('^ {4}Total formats: ([0-9]+), used formats: ([0-9]+)$'),
             'avg_rec_len': re.compile('^ {4}Average record length: ([0-9\.]+), total records: ([0-9]+)'),
@@ -139,6 +141,7 @@ class TableStat:
 
     def reset_stat(self):
         self.name = None
+        self.tabel_space = None
         self.rec_avg_len = None
         self.rec_total = None
         self.ver_avg_len = None
@@ -170,6 +173,7 @@ class IndexStat:
     def __init__(self):
         """Class init"""
         self.name = None
+        self.tabel_space = None
         self.depth = None
         self.leaf_buckets = None
         self.nodes = None
@@ -189,6 +193,7 @@ class IndexStat:
         self.fill_99 = None
         self.re = {
             'name': re.compile('^ {4}Index (.+) \(([0-9]+)\)$'),
+            'table_space': re.compile('^\tTablespace: (.+)$'),
             'depth': re.compile('^\t(Root page: [0-9]+, )*depth: ([0-9]+), leaf buckets: ([0-9]+), nodes: ([0-9]+)$',
                                 re.IGNORECASE),
             'avg_data_len': re.compile('^\tAverage data length: ([0-9\.]+), total dup: ([0-9]+), max dup: ([0-9]+)$'),
@@ -206,6 +211,7 @@ class IndexStat:
 
     def reset_stat(self):
         self.name = None
+        self.tabel_space = None
         self.depth = None
         self.leaf_buckets = None
         self.nodes = None
@@ -217,6 +223,23 @@ class IndexStat:
         self.fill_60 = None
         self.fill_80 = None
         self.fill_99 = None
+
+
+class TableSpaces:
+    """Class description here"""
+
+    def __init__(self):
+        """Class init"""
+        self.name = None
+        self.file_name = None
+        self.re = {
+            'name': re.compile('^([^ ]+) \(([0-9]+)\)$'),
+            'file_name': re.compile('^ {4}Full path: (.+)$'),
+        }
+
+    def reset_stat(self):
+        self.name = None
+        self.file_name = None
 
 
 class GStatToSQL:
@@ -250,7 +273,7 @@ class GStatToSQL:
                 pass
             elif line == "    Variable header data:\n":
                 # Skip Variable header data
-                while line != "Analyzing database pages ...\n":
+                while (line != "Analyzing database pages ...\n") and (line != "Tablespaces:\n"):
                     line = fd.readline()
                 break
             else:
@@ -285,6 +308,28 @@ class GStatToSQL:
                         db_header.value['next_header_page'], db_header.value['database_dialect'],
                         db_header.value['attributes']))
         db_id = cursor.fetchone()[0]
+        if line == "Tablespaces:\n":
+            table_spaces = TableSpaces()
+            line = fd.readline()
+            while line != "Analyzing database pages ...\n":
+                if table_spaces.re['name'].search(line):
+                    table_spaces.name = table_spaces.re['name'].search(line).group(1)
+                    table_spaces.id = table_spaces.re['name'].search(line).group(2)
+                    line = fd.readline()
+                    if table_spaces.re['file_name'].search(line):
+                        table_spaces.file_name = table_spaces.re['file_name'].search(line).group(1)
+                        #print(table_spaces.name, " ", table_spaces.id, " ", table_spaces.file_name)
+                elif line == "\n":
+                    if table_spaces.name:
+                        cursor.execute(
+                            "update or insert into TBL_SPC (DB_ID, \"NAME\", FILE_NAME) "
+                            "values (?, ?, ?) "
+                            "matching (DB_ID, \"NAME\", FILE_NAME);",
+                            (db_id, table_spaces.name, table_spaces.file_name))
+                        table_spaces.reset_stat()
+                # Skip list of tables and index in this tablespace
+                line = fd.readline()
+
         table = TableStat()
         tbl_id = None
         index = IndexStat()
@@ -294,6 +339,9 @@ class GStatToSQL:
             if table.re['name'].search(line):
                 table.name = table.re['name'].search(line).group(1)
                 line = fd.readline()
+                if table.re['table_space'].search(line):
+                    table.tabel_space = table.re['table_space'].search(line).group(1)
+                    line = fd.readline()
                 if table.re['ppp'].search(line):
                     line = fd.readline()
                 continue
@@ -375,6 +423,9 @@ class GStatToSQL:
             elif index.re['name'].search(line):
                 index.name = index.re['name'].search(line).group(1)
                 line = fd.readline()
+                if index.re['table_space'].search(line):
+                    index.tabel_space = index.re['table_space'].search(line).group(1)
+                    line = fd.readline()
                 if index.re['depth'].search(line):
                     index.depth = index.re['depth'].search(line).group(2)
                     index.leaf_buckets = index.re['depth'].search(line).group(3)
@@ -426,7 +477,7 @@ class GStatToSQL:
             elif line == '\n':
                 if table.name:
                     cursor.execute(
-                        "update or insert into TBL (DB_ID, \"NAME\", REC_AVG_LEN, REC_TOTAL, "
+                        "update or insert into TBL (DB_ID, \"NAME\", TBL_SPC, REC_AVG_LEN, REC_TOTAL, "
                         "VER_AVG_LEN, VER_TOTAL, VER_MAX, "
                         "FRAG_AVG_LEN, FRAG_TOTAL, FRAG_MAX, "
                         "BLOB_TOTAL, BLOB_TOTAL_LENGTH, BLOB_PAGES, "
@@ -437,10 +488,10 @@ class GStatToSQL:
                         "FILL_20, FILL_40, FILL_60, FILL_80, FILL_99, "
                         "FORMATS_TOTAL, FORMATS_USED, AVG_UNPACK_LEN, COMPRESS_RATIO) "
                         "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,"
-                        "?, ?, ?, ?, ?, ?) "
+                        "?, ?, ?, ?, ?, ?, ?) "
                         "matching (DB_ID, \"NAME\") "
                         "returning ID;",
-                        (db_id, table.name, table.rec_avg_len, table.rec_total,
+                        (db_id, table.name, table.tabel_space, table.rec_avg_len, table.rec_total,
                          table.ver_avg_len, table.ver_total, table.ver_max,
                          table.frag_avg_len, table.frag_total, table.frag_max,
                          table.blob_total, table.blob_total_length, table.blob_pages,
@@ -455,13 +506,13 @@ class GStatToSQL:
                 elif index.name:
                     # insert data about index
                     cursor.execute(
-                        "update or insert into IDX (DB_ID, TBL_ID, \"NAME\", DEPTH, LEAF_BUCKETS, NODES, "
+                        "update or insert into IDX (DB_ID, TBL_ID, \"NAME\", TBL_SPC, DEPTH, LEAF_BUCKETS, NODES, "
                         "AVG_LENGTH, DUP_TOTAL, DUP_MAX, AVG_NODE_LEN, AVG_KEY_LEN, "
                         "COMPRESSION_RATIO, AVG_PREFIX_LEN, CLUSTER_FACTOR, CLUSTER_RATIO, "
                         "FILL_20, FILL_40, FILL_60, FILL_80, FILL_99) "
-                        "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) "
+                        "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) "
                         "matching (DB_ID, TBL_ID, \"NAME\");",
-                        (db_id, tbl_id, index.name, index.depth, index.leaf_buckets, index.nodes,
+                        (db_id, tbl_id, index.name, index.tabel_space, index.depth, index.leaf_buckets, index.nodes,
                          index.avg_length, index.dup_total, index.dup_max, index.avg_node_len, index.avg_key_len,
                          index.compression_ratio, index.avg_prefix_len, index.cluster_factor, index.cluster_ratio,
                          index.fill_20, index.fill_40, index.fill_60, index.fill_80, index.fill_99)
